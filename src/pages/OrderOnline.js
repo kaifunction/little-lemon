6e0d53fb7bb5d75673e849c261034988f1f188da
Kai <fengkai0129@gmail.com>
2025-05-04 17:45:51 -0700

    order online

--- src/pages/OrderOnline.js
+++ src/pages/OrderOnline.js
@@ -1,9 +1,22 @@
+import './OrderOnline.css';
+import orderImg from '../images/about-us-pic2.jpg';
+
 function OrderOnline() {
   return (
-    <div  style={{ textAlign: "center", marginTop: "50px" }}>
-      <h1>Order Online</h1>
-      <p>Order your favorite food online!</p>
-    </div>
+    <section className="order-online-container">
+      <div className="order-online-content">
+        <div className="order-online-text">
+          <h2 className="order-title">Order Online</h2>
+          <p className="order-description">
+            Enjoy your favorite dishes from the comfort of your home. Fresh, delicious, and delivered fast!
+          </p>
+          <a href="/order" className="order-button">Start Your Order</a>
+        </div>
+        <div className="order-online-image-wrapper">
+          <img src={orderImg} alt="Order your favorite dish online" className="order-online-image" />
+        </div>
+      </div>
+    </section>
   );
 }
 
