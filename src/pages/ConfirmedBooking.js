b675132d8fed4286f88efa8e0f91d4ab77adf87e
Kai <fengkai0129@gmail.com>
2025-05-06 13:15:32 -0700

    Fix build warning and update site

--- src/pages/ConfirmedBooking.js
+++ src/pages/ConfirmedBooking.js
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import "./ConfirmedBooking.css";
 import { useNavigate, useLocation } from "react-router-dom";
 
